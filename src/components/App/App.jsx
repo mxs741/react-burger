66fe30a77fc23bba98379e3231dcb0cd1cafb49f
AFB <mxsh741@gmail.com>
2023-01-07 21:18:17 +0500

    feat: сверстать header, добавить шрифт

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppHeader from '../App-header/App-header';
+import classes from './app.module.css';
+
+function App() {
+  return (
+    <div className={classes.app}>
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
